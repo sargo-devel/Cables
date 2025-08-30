fe8277a76ca46c3930378b7063e08774d2266459
SargoDevel <sargo-devel@go2.pl>
2025-08-30 14:11:51 +0200

    cableMaterial: adapted to two-color insulation

--- freecad/cables/cableMaterial.py
+++ freecad/cables/cableMaterial.py
@@ -1,71 +1,106 @@
 """cableMaterial
 """
 
+# ***************************************************************************
+# *   Copyright 2024 SargoDevel <sargo-devel at o2 dot pl>                  *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENSE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************/
+
 import os
 import FreeCAD
-import importFCMat
-import Arch
 
-multi_names3 = {'J': 0, 'L1': 2, 'N': 4, 'PE': 5, 'CU': 6}
-multi_names5 = {'J': 0, 'L1': 1, 'L2': 2, 'L3': 3, 'N': 4, 'PE': 5, 'CU': 6}
-multi_names8 = {'J': 0, 'P1A': 9, 'P1B': 10, 'P2A': 1, 'P2B': 7, 'P3A': 11,
-                'P3B': 12, 'P4A': 4, 'P4B': 8, 'CU': 6}
+multi_names3 = [('J', 'InsulJacket'), ('L1', 'InsulBlack'), ('N', 'InsulBlue'),
+                ('PE', 'InsulYellow'), ('PE', 'InsulGreen'), ('CU', 'Cu')]
+
+multi_names5 = [('J', 'InsulJacket'), ('L1', 'InsulBrown'),
+                ('L2', 'InsulBlack'), ('L3', 'InsulGray'), ('N', 'InsulBlue'),
+                ('PE', 'InsulYellow'), ('PE', 'InsulGreen'), ('CU', 'Cu')]
+
+multi_names8 = [('J', 'InsulJacket'), ('P1A', 'InsulOrange'),
+                ('P1B', 'InsulWhite'), ('P1B', 'InsulOrange'),
+                ('P2A', 'InsulBrown'), ('P2B', 'InsulWhite'),
+                ('P2B', 'InsulBrown'), ('P3A', 'InsulGreen'),
+                ('P3B', 'InsulWhite'), ('P3B', 'InsulGreen'),
+                ('P4A', 'InsulBlue'), ('P4B', 'InsulWhite'),
+                ('P4B', 'InsulBlue'), ('CU', 'Cu')]
+
 multi_list = {'cableMultiMat3w': multi_names3, 'cableMultiMat5w': multi_names5,
               'cableMultiMat8w': multi_names8}
-mat_table = [
-    {'name': 'InsulJacket',
-     'mat_name': 'PVC-Generic',
-     'color': (215, 251, 255)
-     },
-    {'name': 'InsulBrown',
-     'mat_name': 'PVC-Generic',
-     'color': (80, 40, 0)
-     },
-    {'name': 'InsulBlack',
-     'mat_name': 'PVC-Generic',
-     'color': (54, 54, 54)
-     },
-    {'name': 'InsulGray',
-     'mat_name': 'PVC-Generic',
-     'color': (160, 160, 160)
-     },
-    {'name': 'InsulBlue',
-     'mat_name': 'PVC-Generic',
-     'color': (0, 85, 255)
-     },
-    {'name': 'InsulYellow',
-     'mat_name': 'PVC-Generic',
-     'color': (255, 255, 0)
-     },
-    {'name': 'Cu',
-     'mat_name': 'Copper-Generic',
-     'color': (184, 115, 51)
-     },
-    {'name': 'InsulLightBrown',
-     'mat_name': 'PVC-Generic',
-     'color': (136, 97, 58)
-     },
-    {'name': 'InsulLightBlue',
-     'mat_name': 'PVC-Generic',
-     'color': (135, 197, 255)
-     },
-    {'name': 'InsulOrange',
-     'mat_name': 'PVC-Generic',
-     'color': (243, 112, 66)
-     },
-    {'name': 'InsulLightOrange',
-     'mat_name': 'PVC-Generic',
-     'color': (254, 216, 177)
-     },
-    {'name': 'InsulGreen',
-     'mat_name': 'PVC-Generic',
-     'color': (47, 205, 102)
-     },
-    {'name': 'InsulLightGreen',
-     'mat_name': 'PVC-Generic',
-     'color': (167, 243, 190)
-     }
-    ]
+
+mat_table = {
+    'InsulJacket': {
+        'mat_name': 'PVC-Generic',
+        'color': (215, 251, 255)
+    },
+    'Cu': {
+        'mat_name': 'Copper-Generic',
+        'color': (184, 115, 51)
+    },
+    'InsulYellow': {
+        'mat_name': 'PVC-Generic',
+        'color': (255, 255, 0)
+    },
+    'InsulBlack': {
+        'mat_name': 'PVC-Generic',
+        'color': (54, 54, 54)
+    },
+    'InsulGray': {
+        'mat_name': 'PVC-Generic',
+        'color': (160, 160, 160)
+    },
+    'InsulBrown': {
+        'mat_name': 'PVC-Generic',
+        'color': (80, 40, 0)
+    },
+    'InsulLightBrown': {
+        'mat_name': 'PVC-Generic',
+        'color': (136, 97, 58)
+    },
+    'InsulBlue': {
+        'mat_name': 'PVC-Generic',
+        'color': (0, 85, 255)
+    },
+    'InsulLightBlue': {
+        'mat_name': 'PVC-Generic',
+        'color': (135, 197, 255)
+    },
+    'InsulGreen': {
+        'mat_name': 'PVC-Generic',
+        'color': (47, 205, 102)
+    },
+    'InsulLightGreen': {
+        'mat_name': 'PVC-Generic',
+        'color': (167, 243, 190)
+    },
+    'InsulOrange': {
+        'mat_name': 'PVC-Generic',
+        'color': (243, 112, 66)
+    },
+    'InsulLightOrange': {
+        'mat_name': 'PVC-Generic',
+        'color': (254, 216, 177)
+    },
+    'InsulWhite': {
+        'mat_name': 'PVC-Generic',
+        'color': (250, 250, 250)
+    }
+}
 
 
 def get_material_from_lib(name):
@@ -80,6 +115,9 @@ def get_material_from_lib(name):
 
 
 def makeMaterial(name, mat_name, color):
+    import importFCMat
+    import Arch
+
     result = get_material_from_lib(mat_name)
     mat = importFCMat.read(result[0]) if result else None
     material = Arch.makeMaterial()
@@ -92,17 +130,23 @@ def makeMaterial(name, mat_name, color):
 
 
 def makeCableMaterials(table=mat_table, multi_list=multi_list):
+    import Arch
+
     materials = []
-    for m in table:
-        materials.append(makeMaterial(m['name'], m['mat_name'], m['color']))
-    multi_names = multi_names3
+    for key in table.keys():
+        materials.append(makeMaterial(key, table[key]['mat_name'],
+                                      table[key]['color']))
+    mat_labels = [m.Label for m in materials]
     for multi_names in multi_list.items():
         multi = Arch.makeMultiMaterial()
         lmat = []
-        for i in multi_names[1].values():
-            lmat.append(materials[i])
+        lnames = []
+        for item in multi_names[1]:
+            idx = mat_labels.index(item[1])
+            lmat.append(materials[idx])
+            lnames.append(item[0])
         multi.Materials = lmat
-        multi.Names = list(multi_names[1].keys())
+        multi.Names = lnames
         thickness = [0.0] * len(multi.Names)
         multi.Thicknesses = thickness
         multi.Label = multi_names[0]

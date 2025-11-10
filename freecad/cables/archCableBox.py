e2030fdb84b21a9cf2fb759f2fc5f35b0932a302
SargoDevel <sargo-devel@go2.pl>
2025-11-10 22:48:02 +0100

    Updated license info

--- freecad/cables/archCableBox.py
+++ freecad/cables/archCableBox.py
@@ -1,6 +1,28 @@
 """ArchCableBox
 """
 
+# ***************************************************************************
+# *   Copyright 2025 SargoDevel <sargo-devel at o2 dot pl>                  *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENSE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************/
+
+
 import os
 from math import pi
 from math import sin
